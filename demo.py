5a80ca11c83b70d2ddceb64573c038627c2c06ea
xzeu <admin@xzeu.com>
2019-07-29 17:33:13 +0800

    去掉key

--- demo.py
+++ demo.py
@@ -3,17 +3,15 @@ import json
 
 '''
 这边可以设置一个默认的appkey和secret，当然也可以不设置
-注意：默认的只需要设置一次就可以了
+注意：默认的只需要设置一次就可以了fa
 '''
-jing.setDefaultAppInfo("110bc5b99cfab32454dcaa8022dd8c4f", "612f41079b0241fd8be42576a845084a")
+jing.setDefaultAppInfo("110bc5b99cfab32454dcaa8022dd8c4", "612f41079b0241fd8be42576a845084")
 
 '''
-使用自定义的域名和端口（测试沙箱环境使用）
-a = top.api.UserGetRequest("gw.api.tbsandbox.com",80)
-使用自定义的域名（测试沙箱环境使用）
-a = top.api.UserGetRequest("gw.api.tbsandbox.com")
+使用自定义的域名和端口（京东没有测试沙箱环境使用）
+a = top.api.UserGetRequest("https://router.jd.com/api",443)
 使用默认的配置（调用线上环境）
-a = top.api.UserGetRequest()
+a = jing.api.UserGetRequest()
 '''
 # a = jing.api.jdUnionOpenCategoryGoodsGet()
 a = jing.api.jdUnionOpenGoodsJingfenQuery()
